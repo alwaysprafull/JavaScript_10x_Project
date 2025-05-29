ed2126903735cbf7fc96ab1ad32d2f055e1e0365
Alwaysprafull <Prafullmishra089@gmail.com>
2025-05-29 22:26:02 +0530

    29 may 2025

---
+++ ex_01_JS_Basics/Lab002.js
@@ -0,0 +1,20 @@
+// Variables
+
+
+// Variables are containers for storing data values. In JavaScript, we can declare variables using:
+
+// var – (old, mostly avoided now)
+
+// let – modern, block-scoped
+
+// const – modern, block-scoped & cannot be reassigned
+
+let name = "Prafull";
+let age = 25;
+
+console.log(name); // Prafull
+console.log(age);
+
+
+
+
